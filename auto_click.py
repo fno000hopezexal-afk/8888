6e34af5b765fe240bef3c5899300b6e3598b4de0
fno000hopezexal-afk <fno000hopezexal@gmail.com>
2026-05-29 19:19:09 +1000

    Update auto_click.py

--- auto_click.py
+++ auto_click.py
@@ -56,7 +56,7 @@ clean_old_logs()
 
 # 主逻辑开始
 try:
-    driver.get("https://dddkaisa.streamlit.app/")
+    driver.get("https://3pftjj.streamlit.app/")
     print("已打开网页，等待页面加载 30 秒...")
     time.sleep(30)  # 初次加载等待
 
